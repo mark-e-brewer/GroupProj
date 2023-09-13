3b177a51c473258ef10deedc255d82757042765f
Mestany2 <121625599+Mestany2@users.noreply.github.com>
2023-09-12 19:53:04 -0500

    Reaction post table updates

--- Models/Reaction.cs
+++ Models/Reaction.cs
@@ -1,10 +1,15 @@
-﻿namespace GroupProj.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace GroupProj.Models
+   
 {
     public class Reaction
     {
         public int Id { get; set; }
         public string? Label { get; set; }
-        public string? ImageURL { get; set; }    
+        public string? ImageURL { get; set; }
+        [DeleteBehavior(DeleteBehavior.Restrict)]
         public ICollection<Posts> Posts { get; set; }
         public int RareUsersId { get; set; }
         public RareUsers RareUsers { get; set; }
